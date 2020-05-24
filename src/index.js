656ef591402f18890eb0d40d8f178f150eafa194
danillu <48914278+Raisess@users.noreply.github.com>
2020-05-23 21:18:14 -0300

    created config json and credencials json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://web.whatsapp.com/');
+// importando o arquivo de configuração
+const { classroom_url } = require('./config.json');
 
+let yourEmail, yourPassword;
+
+// importando o arquivo de credenciais
+if(require('./credencials.json')) {
+  const { email, password } = require('./credencials.json');
   
-  
+  yourEmail = email;
+  yourPassword = password;
+} else {
+  const { email, password } = require('./credencials.template.json');
+
+  yourEmail = email;
+  yourPassword = password;
+}
+
+(async () => {
+  // create a browser instace
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--window-size=1200,700"],
+  });
+
+  console.log('comecei!');
+
+  const page = await browser.newPage();
+  await page.goto(classroom_url);
+
+  await page.click('input[clss="whsOnd zHQkBf"]');
+  await page.keyboard.type(yourEmail);
+
+  // await page.waitForNavigation();
+
+  // await page.click('div[class="_2S1VP copyable-text selectable-text"]');
+  // await page.keyboard.type('oii');
+
   // await browser.close();
+
+  console.log('terminei!');
 })();
